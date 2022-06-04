13872fc0a7e2a5785d515d91fcf5ccd6fda3154a
Piroli <pforciol@student.42.fr>
2022-06-04 16:56:25 +0000

    Hero section is almost done

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,3 +1,64 @@
+import { useState } from 'react';
+import { Link } from 'react-scroll';
+import profile from '../../../public/images/profile.jpeg';
+
+import './Navigation.scss';
+
 export const Navigation = () => {
-  return <div></div>;
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <nav className={`navigation ${toggle ? 'navigation--active' : ''}`}>
+      <a className="navigation__name" href="./">
+        <img
+          className="navigation__name-image"
+          src={profile}
+          alt="Pierre Forcioli"
+        />
+        Pierre Forcioli
+      </a>
+      <button className="navigation__toggle" onClick={() => setToggle(!toggle)}>
+        <i className={`fa-solid ${toggle ? 'fa-xmark' : 'fa-bars'}`}></i>
+      </button>
+      <div className="navigation__wrapper">
+        <ul className="navigation__list">
+          <li className="navigation__list-item">
+            <Link to="experiences" onClick={() => setToggle(false)}>
+              <i className="navigation__list-item__icon fa-solid fa-briefcase" />
+              Experiences
+            </Link>
+          </li>
+          <li className="navigation__list-item">
+            <i className="navigation__list-item__icon fa-solid fa-graduation-cap" />
+            <Link to="education" onClick={() => setToggle(false)}>
+              Education
+            </Link>
+          </li>
+          <li className="navigation__list-item">
+            <i className="navigation__list-item__icon fa-solid fa-screwdriver-wrench" />
+            <Link to="skills" onClick={() => setToggle(false)}>
+              Skills
+            </Link>
+          </li>
+          <li className="navigation__list-item">
+            <i className="navigation__list-item__icon fa-solid fa-code-branch" />
+            <Link to="projects" onClick={() => setToggle(false)}>
+              Projects
+            </Link>
+          </li>
+          <li className="navigation__list-item">
+            <i className="navigation__list-item__icon fa-solid fa-address-card" />
+            <Link to="contact" onClick={() => setToggle(false)}>
+              Contact
+            </Link>
+            <img
+              className="navigation__list-item__image"
+              src={profile}
+              alt="Pierre Forcioli"
+            />
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -6,6 +6,25 @@ export const Hero = () => {
   return (
     <section id="hero" className="section-hero">
       <Navigation />
+      <div className="section-hero__wrapper">
+        <div className="section-hero__first">
+          <h1 className="section-hero__header">
+            <span>Hey</span> 👋,
+            <br />
+            I'm Pierre
+          </h1>
+          <h2 className="section-hero__subcontent">
+            Software Engineer at{' '}
+            <a
+              href="https://www.holbertonschool.com/"
+              className="section-hero__subcontent-link"
+            >
+              Holberton School
+            </a>
+            , open to work!
+          </h2>
+        </div>
+      </div>
     </section>
   );
 };
