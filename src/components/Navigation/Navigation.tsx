261185d95a0028dbdf10941527a51bf57a05c3f2
Pierre Forcioli <pierre.forcioli.06@gmail.com>
2022-06-04 23:25:04 +0000

    Brand new hero page, radd typewriter feature

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,63 +1,68 @@
-import { useState } from 'react';
-import { Link } from 'react-scroll';
-import profile from '../../../public/images/profile.jpeg';
-
 import './Navigation.scss';
+import profile from '/images/profile.jpeg';
+import { useEffect, useState } from 'react';
 
 export const Navigation = () => {
-  const [toggle, setToggle] = useState(false);
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const handleScrollPosition = () => {
+    setScrollPosition(window.pageYOffset);
+    console.log(window.pageYOffset);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScrollPosition);
+
+    return () => {
+      window.removeEventListener('scroll', handleScrollPosition);
+    };
+  }, []);
 
   return (
-    <nav className={`navigation ${toggle ? 'navigation--active' : ''}`}>
-      <a className="navigation__name" href="./">
-        <img
-          className="navigation__name-image"
-          src={profile}
-          alt="Pierre Forcioli"
-        />
-        Pierre Forcioli
-      </a>
-      <button className="navigation__toggle" onClick={() => setToggle(!toggle)}>
-        <i className={`fa-solid ${toggle ? 'fa-xmark' : 'fa-bars'}`}></i>
-      </button>
+    <nav className={`navigation${scrollPosition <= 500 ? '' : ' visible'}`}>
       <div className="navigation__wrapper">
-        <ul className="navigation__list">
-          <li className="navigation__list-item">
-            <Link to="experiences" onClick={() => setToggle(false)}>
-              <i className="navigation__list-item__icon fa-solid fa-briefcase" />
-              Experiences
-            </Link>
-          </li>
-          <li className="navigation__list-item">
-            <i className="navigation__list-item__icon fa-solid fa-graduation-cap" />
-            <Link to="education" onClick={() => setToggle(false)}>
-              Education
-            </Link>
-          </li>
-          <li className="navigation__list-item">
-            <i className="navigation__list-item__icon fa-solid fa-screwdriver-wrench" />
-            <Link to="skills" onClick={() => setToggle(false)}>
-              Skills
-            </Link>
-          </li>
-          <li className="navigation__list-item">
-            <i className="navigation__list-item__icon fa-solid fa-code-branch" />
-            <Link to="projects" onClick={() => setToggle(false)}>
-              Projects
-            </Link>
-          </li>
-          <li className="navigation__list-item">
-            <i className="navigation__list-item__icon fa-solid fa-address-card" />
-            <Link to="contact" onClick={() => setToggle(false)}>
-              Contact
-            </Link>
-            <img
-              className="navigation__list-item__image"
-              src={profile}
-              alt="Pierre Forcioli"
-            />
-          </li>
-        </ul>
+        <div className="navigation__identity">
+          <img
+            src={profile}
+            alt="Pierre Forcioli"
+            className="navigation__identity-picture"
+            width={300}
+            height={300}
+          />
+          <a href="" className="navigation__identity-name">
+            Pierre Forcioli
+          </a>
+        </div>
+        <div className="navigation__socials">
+          <ul className="navigation__socials-list">
+            <li className="navigation__socials-list__item">
+              <a
+                href="https://linkedin.com/in/pierreforcioli"
+                target="_blank"
+                className="navigation__sociale-list__item-link"
+              >
+                <i className="fa-brands fa-linkedin-in"></i>
+              </a>
+            </li>
+            <li className="navigation__socials-list__item">
+              <a
+                href="https://github.com/pforciol"
+                target="_blank"
+                className="navigation__sociale-list__item-link"
+              >
+                <i className="fa-brands fa-github"></i>
+              </a>
+            </li>
+            <li className="navigation__socials-list__item">
+              <a
+                href="https://twitter.com/pierreforcioli"
+                target="_blank"
+                className="navigation__sociale-list__item-link"
+              >
+                <i className="fa-brands fa-twitter"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -1,28 +1,67 @@
 import './Hero.scss';
-
+import profile from '/images/profile.jpeg';
 import { Navigation } from '../../components';
+
+import Typewriter from 'typewriter-effect';
 
 export const Hero = () => {
   return (
     <section id="hero" className="section-hero">
       <Navigation />
       <div className="section-hero__wrapper">
-        <div className="section-hero__first">
-          <h1 className="section-hero__header">
-            <span>Hey</span> 👋,
-            <br />
-            I'm Pierre
-          </h1>
-          <h2 className="section-hero__subcontent">
-            Software Engineer at{' '}
-            <a
-              href="https://www.holbertonschool.com/"
-              className="section-hero__subcontent-link"
-            >
-              Holberton School
-            </a>
-            , open to work!
-          </h2>
+        <div className="section-hero__tagline">
+          <img
+            src={profile}
+            alt="Pierre Forcioli"
+            className="section-hero__tagline-picture"
+            width={300}
+            height={300}
+          />
+          <div className="section-hero__tagline-text">
+            <span className="section-hero__tagline-text-name">
+              Pierre Forcioli
+            </span>
+            <div className="section-hero__tagline-text-job">
+              Software Engineer at{' '}
+              <a href="https://holbertonschool.com" target="_blank">
+                <span className="section-hero__tagline-text-job__link">
+                  Holberton School
+                </span>
+              </a>
+              .
+            </div>
+          </div>
+        </div>
+        <div className="section-hero__content">
+          <div className="section-hero__content-me">
+            <i className="fa-solid fa-terminal" />
+            <p className="section-hero__content-me-text typewriter">I am</p>
+            <div className="section-hero__content-me-text typewriter">
+              <Typewriter
+                options={{
+                  strings: [
+                    'available for work',
+                    'really curious',
+                    'in love with the coffee',
+                    'addicted to cats',
+                  ],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
+            </div>
+            <p className="section-hero__content-me-text normal">
+              I am available for work
+            </p>{' '}
+          </div>
+          <div className="section-hero__content-buttons">
+            <button className="section-hero__content-buttons__button">
+              Get in touch <i className="fa-solid fa-rocket" />
+            </button>
+            <button className="section-hero__content-buttons__button inverted">
+              Learn more <i className="fa-solid fa-arrow-down" />
+            </button>
+          </div>
         </div>
       </div>
     </section>
