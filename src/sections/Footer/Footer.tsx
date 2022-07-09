4a56873991229ce630732c29647d95013df9c300
Pierre Forcioli <pierre.forcioli.06@gmail.com>
2022-07-09 09:55:21 +0000

    Add resume button infooter

--- src/sections/Footer/Footer.tsx
+++ src/sections/Footer/Footer.tsx
@@ -4,11 +4,23 @@ export const Footer = () => {
   return (
     <section id="footer" className="section-footer">
       <p className="section-footer__wrapper">
-        Made with <i className="fa-solid fa-heart" /> by{' '}
-        <a href="https://linkedin.com/in/pierreforcioli" target="_blank">
-          Pierre Forcioli{' '}
-        </a>
-        &copy; {new Date().getFullYear()}
+        <div className="resume">
+          <a
+            href="/files/CV - Pierre Forcioli - Software Engineer.pdf"
+            download
+            target="_blank"
+            className="navigation__sociale-list__item-link"
+          >
+            <i className="fa-solid fa-file-arrow-down"></i> Download my resume
+          </a>
+        </div>
+        <div>
+          Made with <i className="fa-solid fa-heart" /> by{' '}
+          <a href="https://linkedin.com/in/pierreforcioli" target="_blank">
+            Pierre Forcioli{' '}
+          </a>
+          &copy; {new Date().getFullYear()}
+        </div>
       </p>
     </section>
   );
